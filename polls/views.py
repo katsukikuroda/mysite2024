410e0c30305d4eb7367dd3302b4f3d2b36adec43
katsukikuroda <katsukikuroda@gmail.com>
2024-08-27 17:02:01 +0900

    14view cmt

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    return HttpResponse('pollsのトップページ')
+
+# プチ演習
+def test(request):
+    return HttpResponse('testページ')
