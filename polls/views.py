291ba4b09a82facc9d2a98498d6452c317634f9d
katsukikuroda <katsukikuroda@gmail.com>
2024-08-31 12:08:32 +0900

    15temp cmt

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'polls/index.html')
+    return render(request, 'polls/index.html', {
+        'name': 'びえんじ',
+        # 'money': f'{500 * 23}円',
+        'money': str(500 * 23) + '円',
+        })
