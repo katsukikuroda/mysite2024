0be057ac19a81276875be20079960a9f46539c9e
katsukikuroda <katsukikuroda@gmail.com>
2024-09-05 06:18:35 +0900

    15temp3 cmt

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,19 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'polls/index.html', {
         'name': 'びえんじ',
-        # 'money': f'{500 * 23}円',
-        'money': str(500 * 23) + '円',
-        })
+        'money': 500 * 23,
+    }
+)
+
+favorite_list = ['アイスクリーム', 'ソフトクリーム', 'シュークリーム']
+
+def test(request):
+    context = {
+        'name': 'びえんじ',
+        'age': 17,
+        'favorite': favorite_list,
+    }
+    return render(request, 'polls/test.html', context)
+
+
+
