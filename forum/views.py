8ac8bbfa39de0517edd7ee594eab6148f2c39899
katsukikuroda <katsukikuroda@gmail.com>
2024-09-05 16:18:07 +0900

    16e1ver2 cmt

--- forum/views.py
+++ forum/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+topic_list = ['HTML', 'CSS', 'JavaScript', 'Python', 'Java', 'C', 'PHP', 'R', 'Swift', 'Kotlin']
+
 def index(request):
-    return render(request, 'forum/index.html')
+    context = {
+        'topics': topic_list,
+    }
+    return render(request, 'forum/index.html', context)
