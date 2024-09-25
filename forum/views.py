c886f662f2668aca2bbfa4735ae5a4f1e7768a81
katsukikuroda <katsukikuroda@gmail.com>
2024-09-26 01:45:40 +0900

    21e2 cmt

--- forum/views.py
+++ forum/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Topic
+from django.shortcuts import get_object_or_404, render
+from .models import Message, Topic
 
 def index(request):
     TOPIC_LIST = Topic.objects.all()
@@ -9,7 +9,19 @@ def index(request):
     return render(request, "forum/index.html", context)
 
 def forum(request, topic):
+    topic_obj = get_object_or_404(Topic, name=topic)
+    messages_obj = Message.objects.filter(topic=topic_obj).order_by('created_at')
+    try:
+        new_message = request.POST["send_message"]
+        Message.objects.create(
+            topic=topic_obj,
+            content=new_message,
+        )
+    except:
+        pass
+
     context = {
-        "topic": topic,
+        "topic": topic_obj,
+        "messages": messages_obj,
     }
     return render(request, "forum/forum.html", context)
